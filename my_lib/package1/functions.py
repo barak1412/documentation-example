be5a20784b406b18975d68c1080937df8ae81a48
barak1412 <barak1413@gmail.com>
2024-07-15 18:37:23 +0300

    first func

--- my_lib/package1/functions.py
+++ my_lib/package1/functions.py
@@ -1,5 +1,20 @@
 
 def func1(a: int, b: int) -> int:
+    """
+    Return the addition of two ints.
+
+    Parameters
+    ----------
+    a
+        The first number.
+    b
+        The second number.
+
+    Examples
+    --------
+    >>> func1(4, 5)
+    9
+    """
     return a + b
 
 
